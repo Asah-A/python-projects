b7f8dd278fdcbabf6aa3c4a4eb87cd93c324e409
Asah <47458203+Asah-A@users.noreply.github.com>
2020-01-07 20:08:05 +0000

    Add files via upload

---
+++ CS1117_LAB1_Lecture4/what_is_my_name.py
@@ -0,0 +1,9 @@
+def what_is_my_name():
+    first_name = input("Please enter first name: ")
+    surname = input("Please enter surname name: ")
+
+    print(first_name.capitalize(), surname.capitalize())
+
+what_is_my_name()
+
+
---
+++ CS1117_LAB1_Lecture4/what_is_my_type.py
@@ -0,0 +1,9 @@
+def what_is_my_type(spiderman):
+
+
+
+    print("The value passed was" + type(spiderman))
+what_is_my_type(7)
+what_is_my_type(7,6,7,8)
+what_is_my_type(cow)
+what_is_my_type([4,6,7])
